4a68dd0e6c3353a978b613bba6f5c463932305b6
Forrest Bloom <forrestbloomsound@gmail.com>
2018-10-10 17:52:02 -0400

    Update js/bloom.js

--- js/bloom.js
+++ js/bloom.js
@@ -1,5 +1,10 @@
 new TypeIt('#intro', {
-    strings: 'This is a simple string.',
     speed: 50,
-    autoStart: true
-  });
+    autoStart: false
+    })
+    .type('Forrest Bloom')
+    .pause(100)
+    .break()
+    .type('Music Production, Mixing, and Mastering')
+    .pause(1000)
+    .delete(2);
