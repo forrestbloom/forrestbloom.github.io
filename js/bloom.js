f3b65595770f8da2a727478a134d29e42e31e0df
Forrest Bloom <forrestbloomsound@gmail.com>
2018-10-10 18:07:32 -0400

    adjust max width

--- js/bloom.js
+++ js/bloom.js
@@ -1,16 +1,16 @@
 new TypeIt('#intro', {
-    speed: 120,
+    speed: 85,
     autoStart: false
     })
     .type('Forrest Bloom')
     .pause(100)
     .break()
     .type('Music Production, Mixing, and Mastering')
-    .pause(600)
+    .pause(1000)
     .type(' (just a bit)')
-    .pause(600)
+    .pause(1000)
     .break()
     .type('Checkout my production catalog below')
     .pause(1500)
     .delete(36)
-    .type('I promise most of them are fire');
+    .type('Thanks for listening');
